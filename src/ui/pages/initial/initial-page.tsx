56183500d83e752db68c2880f76d412ad45b9331
Gabriel Ribeiro Ramos <104779676+Kyluar@users.noreply.github.com>
2025-10-22 10:11:27 -0300

    Foundation 1

---
+++ src/ui/pages/initial/initial-page.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Init | Tic Tac Toe',
+}
+
+export default function InitialPage() {
+  return <div>Initial Page</div>
+}
